4b7ea1f15d650a419e0f17c7294c90e02098a250
Dmax007 <maxwelldexter31@yahoo.com>
2020-09-17 11:20:44 +0000

    Created the database

---
+++ EmployeeList/EmployeeList/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeList.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<tblSkill> tblSkills { get; set; }
+        public DbSet<tblEmployee> tblEmployees { get; set; }
+    }
+}
