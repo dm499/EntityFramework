f4a0f554ca5bc203bc25df764aaf59a43fd6e560
Dmax007 <maxwelldexter31@yahoo.com>
2020-09-18 13:50:48 +0000

    Modify the gender page

--- WebApplication/WebApplication/Pages/Gender/Create.cshtml.cs
+++ WebApplication/WebApplication/Pages/Gender/Create.cshtml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using WebApplication.Models;
 
 namespace WebApplication.Pages.Gender
@@ -26,8 +22,6 @@ namespace WebApplication.Pages.Gender
         [BindProperty]
         public GenderTypes GenderTypes { get; set; }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
--- WebApplication/WebApplication/Pages/Gender/Edit.cshtml.cs
+++ WebApplication/WebApplication/Pages/Gender/Edit.cshtml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebApplication.Models;
 
@@ -38,8 +35,6 @@ namespace WebApplication.Pages.Gender
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
